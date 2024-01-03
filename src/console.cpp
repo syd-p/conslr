55164e5aa02ec2c96cd6f58abe56e62564a70640
sydney <syd.m.phillips0@gmail.com>
2024-01-02 20:08:06 -0500

    initial commit

---
+++ src/console.cpp
@@ -0,0 +1,1 @@
+#include "conslr.hpp"
---
+++ tests/main.cpp
@@ -0,0 +1,10 @@
+#include "conslr.hpp"
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Test" << std::endl;
+
+    return 0;
+}
