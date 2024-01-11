201d12c4726b2e370c22cf7e4549078d493f8616
sydney <syd.m.phillips0@gmail.com>
2024-01-11 15:02:48 -0500

    add includes

---
+++ include/conslr/conslr.hpp
@@ -0,0 +1,11 @@
+///
+///@file conslr/conslr.hpp
+///@brief General include header for conslr
+///
+#pragma once
+
+#include "conslr/console.hpp"
+#include "conslr/screen.hpp"
+#include "conslr/theme.hpp"
+#include "conslr/widget.hpp"
+#include "conslr/widgetmanager.hpp"
